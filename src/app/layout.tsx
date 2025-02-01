b1596ccb4b64f93b1a67f9d947ed570f1bca70c7
itsnoproblem <noproblem@mulligan.xyz>
2025-02-01 13:35:28 -0500

    fix favicon 500

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,11 +27,11 @@ export const metadata: Metadata = {
   title: "howfucked.lol",
   description: "How fucked are we, really?",
   icons: {
-    apple: '/apple-touch-icon.png',
     icon: [
       { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
       { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
     ],
+    apple: '/apple-touch-icon.png',
     other: [
       {
         rel: 'manifest',
